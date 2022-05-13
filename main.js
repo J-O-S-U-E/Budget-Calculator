c943aa471194ec0118b292c439a563ee107918bf
J-O-S-U-E <josue.rojas.cs@gmail.com>
2022-05-12 23:00:17 -0700

    added clear btn and delete btn. also refactored and made clearer names

--- main.js
+++ main.js
@@ -1,28 +1,41 @@
 
 // grabs the number from text field and creates a new input text field with the value
 function addExpense() {
-    if (document.getElementById("expense").value == "") {
+    if (document.getElementById("expense-to-add").value == "") { // validation check
         alert("Enter a number");
     } else {
-        let j = parseInt(document.getElementById("expense").value);
-    var i = document.createElement("input");
-    i.setAttribute('type', 'text');
-    i.setAttribute('value', j);
-    document.getElementById("container").appendChild(i);
-    document.getElementById("container").appendChild(document.createElement("br"));
-    document.getElementById("expense").value = "";
+        // EXPENSE CONTAINER CREATION
+        let expenseContainer = document.createElement('div');
+
+        // EXPENSE INPUT FIELD CREATION
+        let expenseValue = parseFloat(document.getElementById("expense-to-add").value);
+        let expenseInputField = document.createElement("input");
+        expenseInputField.setAttribute('type', 'text');
+        expenseInputField.setAttribute('value', expenseValue);
+        expenseContainer.appendChild(expenseInputField);
+        document.getElementById("expense-to-add").value = ""; // clear input field after inputting and creating the new input field
+
+        // EXPENSE DELETE BUTTON CREATION
+        let deleteBtn = document.createElement("BUTTON");
+        deleteBtn.textContent = "delete";
+        expenseContainer.appendChild(deleteBtn);
+        deleteBtn.addEventListener("click", () => { expenseContainer.remove(); });
+
+        // ADD EXPENSE CONTAINER TO CONTAINER WITH ALL EXPENSES
+        document.getElementById("container-all-expenses").appendChild(expenseContainer);
     }
 }
 
-// inputs all values in an array from id="container" with input field. Then adds them all up using a loop and updates html element with total.
+// inputs all values in an array from id="container-all-expenses" with input field. Then adds them all up using a loop and updates html element with total.
 function calculate() {
     let total = 0;
-    let expenses = [];
-    $("#container input").each(function() {
-          expenses.push($(this).val());
-      })
-    for (let i=0; i<expenses.length; i++) {
-        total += parseInt(expenses[i]);
-    }
+    $("#container-all-expenses input").each(function() {
+        total += parseFloat($(this).val());
+    })
     document.getElementById("total").innerHTML = total;
+}
+
+function clearContainer() {
+    $("#container-all-expenses").empty();
+    document.getElementById("total").innerHTML = 0;
 }
